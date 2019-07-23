6d03f5888b2f594ceb733d2298a7f98cb26597ce
엄길용 <dev.ekyong@gmail.com>
2019-07-23 10:02:28 +0900

    Updates

---
+++ precache-manifest.8acc2d08fb70a124129d4578da2cc9ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eec0519c8972bc3508f9",
+    "url": "/portfolio/static/css/main.fa077700.chunk.css"
+  },
+  {
+    "revision": "eec0519c8972bc3508f9",
+    "url": "/portfolio/static/js/main.eec0519c.chunk.js"
+  },
+  {
+    "revision": "8989599310ef6731daac",
+    "url": "/portfolio/static/js/1.89895993.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "a6b63f17ab42f6bab989de7e05dca09d",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b8922c5a9861b1ac93f23f667c2b4abe.js"
+  "/portfolio/precache-manifest.8acc2d08fb70a124129d4578da2cc9ff.js"
 );
 
 workbox.clientsClaim();
